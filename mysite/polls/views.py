a4cbc40756f0d2e5d43189353d93e301862b7dbe
Lachlan Clatworthy <lachlanclatworthy@proton.me>
2024-05-12 17:20:52 +1000

    Replace render calls with generic views for polls

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,21 +1,29 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.db.models import F
 from django.urls import reverse
+from django.views import generic
 
 from .models import Question, Choice
 
-def index(request):
-    context = {"latest_questions" : Question.objects.order_by("-pub_date")[:5]}
-    return render(request, "polls/index.html", context)
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_questions"
 
-def detail(request, question_id):
-    q = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": q})
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by("-pub_date")[:5]
 
-def results(request, question_id):
-    q = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question": q})
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
+
 
 def vote(request, question_id):
     q = get_object_or_404(Question, pk=question_id)
