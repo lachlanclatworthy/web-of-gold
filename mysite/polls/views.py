f33d7b180608f7d7265068c29b90006701c68158
Lachlan Clatworthy <lachlanclatworthy@proton.me>
2024-05-12 16:48:09 +1000

    Add choice submission for a given question
    Redirects to the question's results page after a vote is submitted.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.db.models import F
+from django.urls import reverse
 
-from .models import Question
+from .models import Question, Choice
 
 def index(request):
     context = {"latest_questions" : Question.objects.order_by("-pub_date")[:5]}
@@ -16,4 +18,19 @@ def results(request, question_id):
     return HttpResponse(response % question_id)
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    q = get_object_or_404(Question, pk=question_id)
+
+    try:
+        selected_choice = q.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        # Go back to the question voting form
+        return render(request, "polls/detail.html",
+                      {
+                          "question": q,
+                          "error_message": "You need to select a choice.",
+                      })
+    else:
+        selected_choice.votes = F('votes') + 1
+        selected_choice.save()
+        # NOTE - We avoid a potential double POST by issuing a redirect
+        return HttpResponseRedirect(reverse("polls:results", args=(q.id,)))
