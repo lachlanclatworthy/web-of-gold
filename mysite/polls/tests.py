77afe3e21e1568c9fdb387a16451f5a04af98801
Lachlan Clatworthy <lachlanclatworthy@proton.me>
2024-05-14 20:47:14 +1000

    Detail view won't display for future questions

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -98,3 +98,24 @@ class QuestionIndexViewTests(TestCase):
                 response.context["latest_questions"],
                 [q2, q1],
         )
+
+class QuestionDetailViewTests(TestCase):
+    def test_future_questions(self):
+        """
+        Detail view for future questions returns 404
+        """
+        future_question = create_question(question_text="Future question", days=+5)
+        url = reverse("polls:detail", args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+
+    def test_past_question(self):
+        """
+        Detail view of a question with a pub_date in the past
+        displays the question's text
+        """
+        past_question = create_question(question_text="Past question", days=-5)
+        url = reverse("polls:detail", args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
